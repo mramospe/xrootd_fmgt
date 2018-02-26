beb8e6c7f14c04a11300c348128f12001b8d4fed
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2018-02-26 21:17:05 +0100

    Initialize repository

---
+++ tests/test_core.py
@@ -0,0 +1,49 @@
+'''
+Test function for the "core" module.
+'''
+
+__author__ = ['Miguel Ramos Pernas']
+__email__  = ['miguel.ramos.pernas@cern.ch']
+
+# Custom
+import xrootd_fmg
+
+
+def test_get_mtime():
+    '''
+    Create a file and get the modification time.
+    '''
+    make_fname = lambda i, 'dummy_{}.txt'.format(i)
+    fname = 'dummy_0.txt'
+
+    i = 0
+    while os.path.exists(fname):
+        i += 1
+        fname = make_fname(i)
+
+    with open(fname, 'wt') as f:
+        xrootd_fmg._get_mtime(fname(i))
+
+    os.remove(fname)
+
+
+def test_remote_name():
+    '''
+    Test the remote naming scheme.
+    '''
+
+    # XROOTD
+    assert xrootd_fmg._is_xrootd('root://my-site//')
+
+    assert not xrootd_fmg._is_xrootd('my-site')
+
+    s, p = xrootd_fmg.split_remote('root://my-site//path/to/file')
+    assert s == 'my-site' and p == 'path/to/file'
+
+    # SSH
+    assert xrootd_fmg._is_ssh('username@server')
+
+    assert not xrootd_fmg._is_ssh('username-server')
+
+    s, p = xrootd_fmg.split_remote('user@my-site:path/to/file')
+    assert s == 'user@my-site' and p == 'path/to/file'
