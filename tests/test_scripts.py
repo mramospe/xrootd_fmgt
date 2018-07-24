a4fe3eaf8b196914cd7c3107ce325ca1a1aa75c6
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2018-07-24 12:23:57 +0200

    Modify script so one can add files from a directory matching a regular expression

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -17,19 +17,41 @@ def test_hep_rfm_table( tmpdir ):
     '''
     Test function for the "hep-rfm-table" script.
     '''
+    # Add file in the current directory
     file1_path = tmpdir.join('file1.txt')
     file2_path = tmpdir.join('file2.txt')
     file3_path = tmpdir.join('file3.txt')
     file4_path = tmpdir.join('file4.txt')
 
+    # Add file into a sub-directory as well
+    subdir = tmpdir.mkdir('subdir')
+    file5_path = subdir.join('file5.txt')
+    file6_path = subdir.join('file6.txt')
+
+    # Create a sub-sub-directory
+    subsubdir = subdir.mkdir('subsubdir')
+    file7_path = subsubdir.join('file7.txt')
+    file8_path = subsubdir.join('file8.db') # Does not end on ".txt"
+
     table_path = tmpdir.join('table.txt')
 
     with file1_path.open('wb') as f:
         f.write(b'file1 text in binary')
+    #
+    # file2 is missing
+    #
     with file3_path.open('wt') as f:
         f.write('file3 text')
     with file4_path.open('wt') as f:
         f.write('file4 text is a bit larger')
+    with file5_path.open('wt') as f:
+        f.write('file5 is in the sub-directory')
+    with file6_path.open('wt') as f:
+        f.write('file6 is in the sub-directory and has a larger text')
+    with file7_path.open('wt') as f:
+        f.write('file7 is in the sub-sub-directory')
+    with file8_path.open('wt') as f:
+        f.write('file8 is in the sub-sub-directory')
 
     cmds = (
         'hep-rfm-table {} create'.format(table_path),
@@ -43,11 +65,25 @@ def test_hep_rfm_table( tmpdir ):
         p = subprocess.Popen(c.split())
         assert p.wait() == 0
 
+    # Add files in directory filtering by regular expression
+    p = subprocess.Popen('hep-rfm-table {} add_from_dir {} --regex .*.txt$'.format(table_path, subdir).split())
+    assert p.wait() == 0
+
     table = hep_rfm.Table.read(table_path.strpath)
 
     for f in table.values():
         f.marks.tmstp != hep_rfm.tables.__default_tmstp__
         f.marks.fid   != hep_rfm.tables.__default_fid__
+
+    assert len(table) == 7
+
+    # Add all files in the sub-directory
+    p = subprocess.Popen('hep-rfm-table {} add_from_dir {}'.format(table_path, subdir).split())
+    assert p.wait() == 0
+
+    table = hep_rfm.Table.read(table_path.strpath)
+
+    assert len(table) == 8
 
     # Commands that break
     break_cmds = (
