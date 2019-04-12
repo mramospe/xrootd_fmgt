9a4d15251d27834b8a5ca08014af176e3d9f8bf5
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2019-04-12 13:04:34 +0200

    Limit the number of displayed characters in hep-rfm-table to 20. Do not display the three dots when the file ID is 'none'. Fixes to documentation.

--- docs/code/table_example.sh
+++ docs/code/table_example.sh
@@ -13,21 +13,21 @@ hep-rfm-table add table.db file1 file1.txt
 hep-rfm-table display table.db
 echo "This is the second file" >> file2.txt
 echo "This is the third file" >> file3.txt
-hep-rfm-table add_massive table.db file2.txt file3.txt
+hep-rfm-table add-massive table.db file2.txt file3.txt
 hep-rfm-table display table.db
 mkdir -p subdir/subsubdir
 echo "This is the fourth file" >> subdir/file4.txt
 echo "This is the fifth file" >> subdir/subsubdir/file5.txt
 echo "This is the sixth file" >> subdir/subsubdir/file6.dt
-hep-rfm-table add_from_dir table.db . --regex .*.txt
+hep-rfm-table add-from-dir table.db . --regex .*.txt
 hep-rfm-table display table.db
-hep-rfm-table add_from_dir table.db .
+hep-rfm-table add-from-dir table.db .
 hep-rfm-table display table.db
 hep-rfm-table remove table.db --files table
 hep-rfm-table display table.db
 mkdir files
 mv subdir *.txt files/.
-hep-rfm-table add_from_dir table.db files
+hep-rfm-table add-from-dir table.db files
 hep-rfm-table display table.db
 hep-rfm-table create rtable.db
 mkdir rfiles
